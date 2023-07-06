aaee7033545fa5d5c116e3fde321948dd88bd48b
Alla-Kalyakina <110158921+Alla-Kalyakina@users.noreply.github.com>
2023-07-06 13:43:38 +0300

    add Webpack files

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    module: { 
+        mode: 'production',
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'main.js',
+        },
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCSSExtractPlugin.loader,
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new MiniCSSExtractPlugin()
+    ]
+}
